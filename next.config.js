f688a7cea9067c0156c1ccd549526957eacd8e2a
Jihan Jasper Al-rashid <69488696+j-jasper-a@users.noreply.github.com>
2023-11-11 14:03:20 +0600

    chore: clean up Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["placehold.co", "picsum.photos"],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
